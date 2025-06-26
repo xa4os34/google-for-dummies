e87713ff75d6da7b58d1b6782111edba669e1f91
xa4os34 <matyushag8907@gmail.com>
2025-06-26 19:11:20 +0500

    idk

--- src/share/Models/CrawlingFeedback.cs
+++ src/share/Models/CrawlingFeedback.cs
@@ -3,18 +3,11 @@ namespace Gfd.Models;
 
 public class CrawlingFeedback 
 {
-    public Type Type { get; init; }
-
-    public required Uri[] Urls { get; init; } = new Uri[0];
-
     public required Uri BaseUrl { get; init; }
 
-}
-
-public class RobotsTxtCrawlingFeedback : CrawlingFeedback
-{
-    public Uri[] AllowedUrls  { get; init; } = new Uri[0];
+    public required Uri[] NewUrls { get; init; } = new Uri[0];
 
     public Uri[] DisallowedUrls  { get; init; } = new Uri[0];
-
+    
+    public TimeSpan? newCrawlingDelay { get; init; }
 }
